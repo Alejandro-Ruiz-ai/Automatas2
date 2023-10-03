f385d4c39ed888708b621b25a3bcb5c76cad09df
Alejandro-Ruiz-ai <124332509+Alejandro-Ruiz-ai@users.noreply.github.com>
2023-10-03 13:20:55 -0600

    Add files via upload

--- suma.cpp
+++ suma.cpp
@@ -1,11 +1,23 @@
-#include<stdio.h>
-#include<math.h>
-#include<iostream>
-#include<conio.h>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
 
-float x26;
+char altura,i,j;
 
 void main() // Funcion principal
 {
-    x26=(3+5)*8-(10-4)/2;
+    printf("\nAltura: ");
+    scanf("&i",&altura);
+
+    for (i = 1; i <= altura; i++)
+    {
+        for (j = 250; j <=250+i; j++)
+        {
+            if (j%2==0)
+                printf("-");
+            else
+                printf("+");
+        }
+        printf("\n");
+    }
 }
